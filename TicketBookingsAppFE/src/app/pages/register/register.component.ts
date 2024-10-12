81840a96ccc82a9add6e92659e8f3bfc166b042d
Naman Saini <naman.s@veersatech.com>
2024-10-12 15:34:59 +0530

    email validation fix in registration form

--- TicketBookingsAppFE/src/app/pages/register/register.component.ts
+++ TicketBookingsAppFE/src/app/pages/register/register.component.ts
@@ -28,7 +28,7 @@ export class RegisterComponent implements OnInit {
   ) {
     this.userRegisterForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(4)]],
-      email: ['', [Validators.required], Validators.email],
+      email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, Validators.pattern('^[0-9]{10,15}$')]],
       password: ['', [Validators.required, Validators.minLength(4)]],
     });
